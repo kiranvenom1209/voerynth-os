b4ccf1024a5131dd4a8087dea2bb577c6054aaf5
IBN <IBN@NB-082.ad.boehm-group.com>
2026-03-04 15:00:46 +0100

    fix: resolve duplicate export in dashboard index.js causing build failure

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -2,7 +2,6 @@ export { default as DashboardHeader } from './DashboardHeader';
 export { default as NeuralInterfaceCard } from './NeuralInterfaceCard';
 export { default as WeatherCard } from './WeatherCard';
 export { default as QuickModeSelector } from './QuickModeSelector';
-export { default as QuickModeSelector } from './QuickModeSelector';
 export { default as EnergyFlowCard } from './EnergyFlowCard';
 export { default as ResidentsCard } from './ResidentsCard';
 
